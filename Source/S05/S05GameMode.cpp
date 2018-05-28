18511e84131578824eb45a9acb83161fddb0afae
DESKTOP-D9VPJH0\vanpa <vpattonjr@gmail.com>
2018-05-27 23:46:14 -0500

    trying to recover

--- Source/S05/S05GameMode.cpp
+++ Source/S05/S05GameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "S05GameMode.h"
 #include "S05HUD.h"
-#include "S05Character.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AS05GameMode::AS05GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- Source/S05/S05HUD.cpp
+++ Source/S05/S05HUD.cpp
@@ -10,7 +10,7 @@
 AS05HUD::AS05HUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
